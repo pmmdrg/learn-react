75a0b981737d03fa0257df3bdae033c9d601b0ba
Pham Minh Man <pmmdrg2605@gmail.com>
2024-04-24 21:36:40 +0700

    Day 3: PropTypes, Memo

--- src/App.js
+++ src/App.js
@@ -2,20 +2,38 @@ import React, { useState } from 'react';
 
 import './App.css';
 import MyComponent from './MyComponent';
+import Todos from './Todos';
 
 function App() {
-  const [isMount, setIsMount] = useState(false);
+  // const [isMount, setIsMount] = useState(false);
+  // return (
+  //   <>
+  //     <button
+  //       onClick={() => {
+  //         setIsMount(!isMount);
+  //       }}
+  //     >
+  //       Toggle
+  //     </button>
+  //     {isMount && <MyComponent className='new-title' />}
+  //   </>
+  // );
+
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(['todo 1', 'todo 2']);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
 
   return (
     <>
-      <button
-        onClick={() => {
-          setIsMount(!isMount);
-        }}
-      >
-        Toggle
-      </button>
-      {isMount && <MyComponent className='my-component' index={1} />}
+      <Todos todos={todos} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
     </>
   );
 }
--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,17 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import './MyComponent.css';
 
-const MyComponent = ({ className, index }) => {
-  const props = {
-    className,
-    index,
-  };
+const MyComponent = ({ className = 'title', index }) => {
   const [count, setCount] = useState(0);
+  const [isActivated, setActivated] = useState(false);
+
   const handleClick = () => {
     setCount(count + 1);
   };
 
-  return <div className='title'>MyComponent</div>;
+  const handleEffect = () => {
+    setActivated(!isActivated);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('activated:', isActivated);
+    }, 3000);
+  }, [isActivated]);
+
+  return (
+    <div className={className}>
+      <button onClick={handleClick}>Click me {count} times!</button>
+      <button onClick={handleEffect}>Click for activate effect</button>
+    </div>
+  );
+};
+
+MyComponent.propTypes = {
+  className: PropTypes.string.isRequired,
 };
 
 export default MyComponent;
